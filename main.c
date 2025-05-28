db91fb6c687c877c2fa9bf2c16ac29d54e641c2f
Laurin Kerkloh <laurin@kerk-loh.de>
2025-05-28 17:22:14 +0200

    add drawable

--- main.c
+++ main.c
@@ -20,23 +20,70 @@
 #define GAME_WIDTH 60
 #define GAME_HEIGHT 30
 
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ESC 27
-#define FULL_BLOCK "\xE2\x96\x88"
-struct Point {
+
+// OUTPUT MODES
+#define BOLD 1
+#define DIM 2
+#define ITALIC 3
+#define UNDERLINE 4
+#define BLINKING 5
+#define STRIKETHROUGH 9
+
+// COLORS
+#define BACKGROUND(c) (c + 10)
+#define BLACK 30
+#define RED 31
+#define GREEN 32
+#define YELLOW 33
+#define BLUE 34
+#define MAGENTA 35
+#define CYAN 36
+#define WHITE 37
+#define DEFAULT 39
+
+#define FULL_BLOCK "\xE2\x96\x88\0"
+#define SMILING_FACE "\xE2\x98\xBB\0"
+#define SQUARE "\xE2\x96\xA0\0"
+
+struct Vector {
   int32_t x;
   int32_t y;
 };
 
-struct Point screen_size;
+struct Display {
+  char character[5];
+  char modes[10];
+};
 
-//////////////////////////////////
-// TERMINAL AND IO SETTINGS
-//////////////////////////////////
+struct Drawable {
+  struct Vector position;
+  struct Display display;
+};
 
+//////////////////////
+// Global Variables //
+//////////////////////
 char output_buffer[OUTPUT_BUFFER_SIZE];
 struct termios original_termios;
 int flags;
+struct Vector screen_size;
+
+struct FrameInfo recent_frames_data[RECENT_FRAMES_SIZE];
+struct FrameInfoBuffer frame_info;
+
+char input_buffer[20];
+char last_input[sizeof(input_buffer) + 1];
+
+//////////////////////////////
+// TERMINAL AND IO SETTINGS //
+//////////////////////////////
 
 void set_output_buffer(void) {
   setvbuf(stdout, output_buffer, _IOFBF, OUTPUT_BUFFER_SIZE);
@@ -116,15 +163,10 @@ void set_screen_size(void) {
 }
 
 ///////////////////////////////////////////////
-struct FrameInfo recent_frames_data[RECENT_FRAMES_SIZE];
-struct FrameInfoBuffer frame_info;
-
-char input_buffer[20];
-char last_input[sizeof(input_buffer) + 1];
 
 void clear_screen(void) { printf("\033[2J\033[1;1H"); }
 
-void move_cursor_point(struct Point p) { printf("\033[%d;%dH", p.y, p.x); }
+void move_cursor_vector(struct Vector v) { printf("\033[%d;%dH", v.y, v.x); }
 void move_cursor(uint16_t x, uint16_t y) { printf("\033[%d;%dH", y, x); }
 
 void print_frame_info(void) {
@@ -152,10 +194,11 @@ void print_input_info(void) {
 // Game State //
 ////////////////
 
-struct Point level_size = {GAME_WIDTH, GAME_HEIGHT};
-struct Point player = {10, 10};
+struct Vector level_size = {GAME_WIDTH, GAME_HEIGHT};
+struct Vector game_offset;
 
-struct Point game_offset;
+struct Drawable player = {{.x = 10, .y = 10},
+                          {.character = SQUARE, .modes = {GREEN, '\0'}}};
 
 bool set_game_offset(void) {
   if (level_size.x * 2 > screen_size.x || level_size.y > screen_size.y) {
@@ -166,16 +209,31 @@ bool set_game_offset(void) {
   return true;
 }
 
-struct Point game_point_to_terminal(struct Point game) {
-  struct Point p = {game_offset.x + game.x, game_offset.y + game.y};
+struct Vector game_vector_to_terminal(struct Vector game) {
+  struct Vector p = {game_offset.x + game.x, game_offset.y + game.y};
   return p;
 }
 
 void move_cursor_game(int16_t x, int16_t y) {
-  move_cursor_point(game_point_to_terminal((struct Point){x, y}));
+  move_cursor_vector(game_vector_to_terminal((struct Vector){x, y}));
 }
-void move_cursor_game_point(struct Point p) {
-  move_cursor_point(game_point_to_terminal(p));
+void move_cursor_game_vector(struct Vector p) {
+  move_cursor_vector(game_vector_to_terminal(p));
+}
+
+void draw_drawable(struct Drawable drawable) {
+  move_cursor_game_vector(drawable.position);
+
+  printf("\033[");
+  for (uint8_t i = 0; drawable.display.modes[i] != '\0'; i++) {
+    printf("%d", drawable.display.modes[i]);
+    if (drawable.display.modes[i + 1] != '\0') {
+      printf(";");
+    }
+  }
+  printf("m");
+  printf("%s", drawable.display.character);
+  printf("\033[0m");
 }
 
 void draw_border(void) {
@@ -184,10 +242,51 @@ void draw_border(void) {
       if (x == -1 || x == level_size.x + 1 || y == -1 ||
           y == level_size.y + 1) {
         move_cursor_game(x, y);
-        printf(FULL_BLOCK);
+        printf("%s", FULL_BLOCK);
       }
     }
   }
+}
+
+struct Vector vector_from_direction(uint8_t direction, uint8_t distance) {
+  struct Vector result;
+  switch (direction) {
+  case UP:
+    result.x = 0;
+    result.y = -distance;
+    break;
+  case DOWN:
+    result.x = 0;
+    result.y = distance;
+    break;
+  case LEFT:
+    result.x = -distance;
+    result.y = 0;
+    break;
+  case RIGHT:
+    result.x = distance;
+    result.y = 0;
+    break;
+  }
+  return result;
+}
+
+struct Vector add_vector(struct Vector a, struct Vector b) {
+  struct Vector result = {a.x + b.x, a.y + b.y};
+  return result;
+}
+
+bool out_off_bounds(struct Vector v) {
+  return (v.x < 0 || v.x > GAME_WIDTH - 1 || v.y < 0 || v.y > GAME_HEIGHT - 1);
+}
+
+bool try_player_move(struct Vector vector) {
+  struct Vector new_location = add_vector(player.position, vector);
+  if (out_off_bounds(new_location)) {
+    return false;
+  }
+  player.position = new_location;
+  return true;
 }
 
 int main(void) {
@@ -213,6 +312,7 @@ int main(void) {
   while (!exited) {
     frame_info.current_frame->start = now();
 
+    // Process input
     ssize_t read_bytes =
         read(STDIN_FILENO, input_buffer, sizeof(input_buffer) - 1);
     input_buffer[read_bytes] = '\0';
@@ -221,6 +321,21 @@ int main(void) {
       last_input[i + 1] = '\0';
 
       switch (input_buffer[i]) {
+      case CTRL_KEY('r'):
+        set_screen_size();
+        break;
+      case 'h':
+        try_player_move(vector_from_direction(LEFT, 1));
+        break;
+      case 'j':
+        try_player_move(vector_from_direction(DOWN, 1));
+        break;
+      case 'k':
+        try_player_move(vector_from_direction(UP, 1));
+        break;
+      case 'l':
+        try_player_move(vector_from_direction(RIGHT, 1));
+        break;
       case ESC: // escape char
         switch (input_buffer[i + 1]) {
         case '[':
@@ -229,23 +344,21 @@ int main(void) {
           exited = true;
           break;
         }
-      case CTRL_KEY('r'):
-        set_screen_size();
-        break;
-      case 'h':
-        break;
       }
     }
-
+    // Draw
     clear_screen();
+
     draw_border();
 
-    print_frame_info();
+    draw_drawable(player);
 
+    print_frame_info();
     print_input_info();
 
     fflush(stdout);
 
+    // Frame end
     frame_info.current_frame->end = now();
 
     wait(until_end_of_frame(frame_info.current_frame->start, FRAME_TIME));
